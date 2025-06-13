78508a6f20eaf4cc0df34aaf41568e45b261cc19
Kailouis <itsmedodhay@gmail.com>
2025-06-13 17:16:10 +0700

    Speed up mp4 conversion

--- services/mp4ConversionService.ts
+++ services/mp4ConversionService.ts
@@ -13,7 +13,17 @@ export const convertWebMToMP4 = async (
     await ffmpeg.load();
   }
   await ffmpeg.writeFile('input.webm', await fetchFile(webmBlob));
-  await ffmpeg.exec(['-i', 'input.webm', '-c:v', 'libx264', '-pix_fmt', 'yuv420p', 'output.mp4']);
+  await ffmpeg.exec([
+    '-i',
+    'input.webm',
+    '-c:v',
+    'libx264',
+    '-preset',
+    'ultrafast',
+    '-pix_fmt',
+    'yuv420p',
+    'output.mp4',
+  ]);
   const data = await ffmpeg.readFile('output.mp4');
   return new Blob([data.buffer], { type: 'video/mp4' });
 };
